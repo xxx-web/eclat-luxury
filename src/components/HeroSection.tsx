ba4bb943f5d53181800ebf3bb1336193ef00a13e
xxx-web <2219054797@qq.com>
2026-06-19 18:44:35 +0800

    fix: 移除 HeroSection 的 hero-bg.jpg 引用和黑色叠加层

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -43,16 +43,6 @@ export function HeroSection() {
         }} />
       </div>
 
-      {/* Background Image */}
-      <div className="absolute inset-0 z-0">
-        <img
-          src="/images/hero-bg.jpg"
-          alt=""
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute inset-0 bg-gradient-to-b from-black/40 via-transparent to-black/80" />
-      </div>
-
       {/* Content */}
       <div className="relative z-20 w-full max-w-7xl mx-auto px-6 md:px-10 lg:px-16">
         <div className="max-w-3xl" style={{ paddingLeft: '5%' }}>
